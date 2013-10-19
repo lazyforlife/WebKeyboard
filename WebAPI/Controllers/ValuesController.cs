40dade4c0a986c6b89def05e8d13f788ef44c5e6
lazyforlife <lime@live.ca>
2013-10-19 19:11:20 -0400

    Rough API + desktop app complete

---
+++ WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using System.Messaging;
+using System.Web.Http;
+
+
+namespace WebAPI.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        string path = @".\Private$\WebKeyboard1";
+        MessageQueue msgQ;
+        public ValuesController()
+            : base()
+        {
+            if (!MessageQueue.Exists(path))
+                MessageQueue.Create(path);
+            msgQ = new MessageQueue(path);
+
+        }
+
+
+        NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        public void Get(int id)
+        {
+            msgQ.Send(id, "One");
+        }
+
+        public void Get(int id, bool shift, bool ctrl, bool alt)
+        {
+            ///TODO
+        }
+
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+    }
+}
---
+++ WebKeyboardDesktop/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Diagnostics;
+using System.Messaging;
+using System.Windows.Forms;
+using WindowsInput;
+
+namespace WebKeyboardDesktop
+{
+    public partial class Form1 : Form
+    {
+        string path = @".\Private$\WebKeyboard1";
+        MessageQueue msgQ;
+        public Form1()
+        {
+            InitializeComponent();
+            notifyIcon1.Text = "WebKeyboard - Desktop";
+            if (!MessageQueue.Exists(path))
+                MessageQueue.Create(path);
+            msgQ = new MessageQueue(path);
+            backgroundWorker1.RunWorkerAsync();
+            backgroundWorker1.ProgressChanged += backgroundWorker1_ProgressChanged;
+        }
+
+        void backgroundWorker1_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            textBox1.Text += MakeHumanString(e.ProgressPercentage) + Environment.NewLine;
+        }
+
+        string MakeHumanString(int keycode)
+        {
+            return string.Format("{0} {1} {2}", DateTime.Now, keycode, ((VirtualKeyCode)keycode).ToString());
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "Muting" + Environment.NewLine;
+            InputSimulator.SimulateKeyPress(VirtualKeyCode.VOLUME_MUTE);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            Debug.WriteLine("Starting background worker");
+            while (!backgroundWorker1.CancellationPending)
+            {
+                System.Messaging.Message msg = msgQ.Receive();
+                if (msg.Label == "One")
+                {
+                    msg.Formatter = new XmlMessageFormatter(new Type[1] { typeof(int) });
+                    Debug.WriteLine(msg.Body);
+                    int t = (int)msg.Body;
+                    backgroundWorker1.ReportProgress(t);
+                    InputSimulator.SimulateKeyPress((VirtualKeyCode)t);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+    }
+}
